ac6529f85fbdd25fcce48ce2c614509f15e1a779
Josh Motta <joshua.a.motta@gmail.com>
2020-11-02 22:04:13 -0500

    init commit

---
+++ src/main/java/com/hello/business/Car.java
@@ -0,0 +1,34 @@
+package com.hello.business;
+
+public class Car {
+	
+	private String make;
+	private String model;
+	
+	public Car() {
+		super();
+	}
+	
+	public Car(String make, String model) {
+		super();
+		this.make = make;
+		this.model = model;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+}
